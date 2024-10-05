2721e905f2f49f23619ec4d55c1a6d31badef10c
NehaDhirendraSirur <152086498+NehaDhirendraSirur@users.noreply.github.com>
2024-10-05 18:33:23 +0530

    Delete UserQuerySystem directory

--- UserQuerySystem/src/main/java/com/example/UserQuerySystem/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UserQuerySystem.Repository;
-
-import com.example.UserQuerySystem.Entity.UserEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserEntity, String> {
-}
